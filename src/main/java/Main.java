64ad105199479990bd52a58eaf1b0e7bcdbfed32
ryujioda <ryuji.oda@gmail.com>
2021-07-27 01:04:14 +0900

    add: example calculation for Big-O

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
 class Main {
-    public static void main(String[]args){
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+        System.out.println(pairSumSequence(5));
+    }
+
+    // O(N)の実行時間とO(N)のメモリ領域が必要
+    // この関数の実行が終わるまで再帰的に呼ばれた関数がスタックするためO(N)のメモリを消費
+    private static int sum(int num) {
+        if (num <= 0) return 0;
+        return num + sum(num - 1);
+    }
+
+    // O(N)の実行時間とO(1)のメモリ領域が必要
+    private static int pairSumSequence(int num) {
+        int sum = 0;
+        for (int i = 0; i < num; i++) {
+            sum += pairSum(i, i + 1);
+        }
+        return sum;
+    }
+
+    // コールスタック上に同時に存在することはないのでメモリはO(1)
+    private static int pairSum(int num1, int num2) {
+        return num1 + num2;
     }
 }
