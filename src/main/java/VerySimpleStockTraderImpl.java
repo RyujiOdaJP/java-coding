e695d8c93d7a60ae310a2b21731a6c2a0521d133
Ryuji Oda <ryuji.oda@gmail.com>
2021-08-03 23:56:01 +0900

    comment

--- src/main/java/VerySimpleStockTraderImpl.java
+++ src/main/java/VerySimpleStockTraderImpl.java
@@ -1,10 +1,14 @@
 public class VerySimpleStockTraderImpl implements IAutomatedStockTrader {
 
+    // 直接IStockAnalysisService, IOnlineBrokerageServiceインタフェースを実装したクラスのインスタンスを作成しており、
+    // これらの実装に深く依存してしまっている。
+    // 呼び出し元で自由に実装クラスは分けられるべき。
     private IStockAnalysisService analysisService = new StockAnalysisServiceImpl();
     private IOnlineBrokerageService brokerageService = new NewYorkStockExchangeBrokerageServiceImpl();
 
     public void executeTrades() {
-
+        analysisService.getEstimatedValue("tesla");
+        brokerageService.getStockSymbols();
     }
 
     private static class NewYorkStockExchangeBrokerageServiceImpl implements IOnlineBrokerageService {
